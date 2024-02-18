f42fcd4bad038082f3b2f00e57d3aa9a32247126
Abbos29 <abbosvaliev2003@gmail.com>
2024-02-18 17:20:03 +0500

    feat: add adaptive mobile burger menu

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Hero.module.scss'
+
+const Hero = () => {
+  return (
+    <section className="hero">
+        <div className="container">
+            <div className="wrapper">
+                <div className="box">
+
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Hero

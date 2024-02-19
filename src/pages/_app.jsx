cb6db5c85e88f5d3e6a5b027bf737c19f8f09da2
BoburXO <boburhatamov7@gmail.com>
2024-02-19 13:29:34 +0500

    footer start!!

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,13 +1,14 @@
-import Header from "@/components/Header/Header";
-import "@/styles/reset.scss";
-import "@/styles/globals.scss";
+import Header from '@/components/Header/Header';
+import '@/styles/reset.scss';
+import '@/styles/globals.scss';
+import Footer from '@/components/Footer/Footer';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Header />
       <Component {...pageProps} />
-
+      <Footer />
     </>
-  )
+  );
 }

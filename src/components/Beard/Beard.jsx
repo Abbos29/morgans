93b6ed6fa2ebce523c6f612619a44bee5ffe9970
BoburXO <boburhatamov7@gmail.com>
2024-02-20 14:36:14 +0500

    create data.db

--- src/components/Beard/Beard.jsx
+++ src/components/Beard/Beard.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
-import s from './Beard.module.scss'
+import React from 'react';
+import s from './Beard.module.scss';
+import Link from 'next/link';
 
-const Beard = ({title}) => {
+const Beard = ({ title }) => {
   return (
     <section className={s.beard}>
-        <div className="container">
-            <div className={s.box}>
-                <h1>{title}</h1>
-            </div>
+      <div className='container'>
+        <div className={s.box}>
+          <h1>{title}</h1>
+          <h3>
+            <Link href={'/'}>Главная </Link>/ {title}
+          </h3>
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Beard
+export default Beard;

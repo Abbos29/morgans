4efb00da4e2375af195aef7936fc7515ebcdfd5d
BoburXO <boburhatamov7@gmail.com>
2024-02-21 13:11:20 +0500

    feat: server add and article bug fixed

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -2,32 +2,33 @@ import React from 'react';
 import s from './Article.module.scss';
 import { data } from '@/db/data';
 
-const Article = () => {
+const Article = ({id}) => {
+  const findArt = data.find((el) => {
+    return id === el?.id
+  })
   return (
     <>
       <section className={s.article}>
         <div className='container'>
-          {data?.map((el) => (
-            <div className={s.item} key={el?.id}>
-              <h1>{el?.title}</h1>
-              <img src={el?.image} alt="Morgan's" />
+            <div className={s.item} key={findArt?.id}>
+              <h1>{findArt?.title}</h1>
+              <img src={findArt?.image} alt="Morgan's" />
               <div className={s.item__wrapper}>
                 <div className={s.left}>
-                  <h2>{el?.subTitle}</h2>
-                  <p>{el?.desc}</p>
-                  <p>{el?.desc2}</p>
-                  <p>{el?.desc3}</p>
+                  <h2>{findArt?.subTitle}</h2>
+                  <p>{findArt?.desc}</p>
+                  <p>{findArt?.desc2}</p>
+                  <p>{findArt?.desc3}</p>
                 </div>
                 <ul className={s.right}>
-                    <li>{el?.txt1}</li>
-                    <li>{el?.txt2}</li>
-                    <li>{el?.txt3}</li>
-                    <li>{el?.txt4}</li>
-                    <li>{el?.txt5}</li>
+                    <li>{findArt?.txt1}</li>
+                    <li>{findArt?.txt2}</li>
+                    <li>{findArt?.txt3}</li>
+                    <li>{findArt?.txt4}</li>
+                    <li>{findArt?.txt5}</li>
                 </ul>
               </div>
             </div>
-          ))}
         </div>
       </section>
     </>

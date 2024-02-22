5c7c0e9c12e45bbc62a51a8a12430b44f4b08c14
Abbos29 <abbosvaliev2003@gmail.com>
2024-02-22 11:51:33 +0500

    feat: add cart

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,13 +8,27 @@ import { useEffect, useState } from 'react'
 const Header = () => {
 
     const [isMenu, setIsMenu] = useState(false)
+    const [isCart, setIsCart] = useState(false)
     const [isScrolled, setIsScrolled] = useState(false);
     const [isSmallScreen, setIsSmallScreen] = useState(false);
 
-
-
     const menuHandle = () => {
         setIsMenu(!isMenu)
+    }
+
+    const cartHandle = () => {
+        setIsCart(!isCart)
+    }
+
+    const cartClosed = () => {
+        setIsCart(false)
+    }
+
+    const closedOverlay = (e) => {
+        // console.log(e.target);
+        if(!e.target.closest('.cart-open')) {
+            setIsCart(false)
+        }
     }
 
     useEffect(() => {
@@ -80,9 +94,113 @@ const Header = () => {
                         </div>
 
                         <div className={s.cart}>
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 128">
+                            <svg onClick={cartHandle} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 128">
                                 <path d="M95.2 65l-54.6 8c-1.6.2-2.8 1.8-2.5 3.4.2 1.5 1.5 2.6 3 2.6.1 0 .3 0 .4 0l54.6-8c9.4-1.4 16.8-8.2 19-17.4l6.9-28.8c.2-.9 0-1.8-.6-2.6-.6-.7-1.4-1.1-2.4-1.1H21.5C18.8 8.7 11.3 2 11 1.7 9.7.7 7.8.8 6.8 2 5.7 3.3 5.8 5.2 7 6.2c.1.1 7.2 6.5 9 18.2l8.7 57.5c1 6.4 6.4 11.1 12.9 11.1H97c1.7 0 3-1.3 3-3s-1.3-3-3-3H37.6c-3.5 0-6.4-2.5-6.9-6l-8.2-54h92.7l-6 25.1C107.6 58.9 102.1 64 95.2 65zM31 114c0 7.2 5.8 13 13 13s13-5.8 13-13-5.8-13-13-13S31 106.8 31 114zM51 114c0 3.9-3.1 7-7 7s-7-3.1-7-7 3.1-7 7-7S51 110.1 51 114zM87 101c-7.2 0-13 5.8-13 13s5.8 13 13 13 13-5.8 13-13S94.2 101 87 101zM87 121c-3.9 0-7-3.1-7-7s3.1-7 7-7 7 3.1 7 7S90.9 121 87 121z" fill="#000" />
                             </svg>
+
+                            <div onClick={closedOverlay} className={`${s.overlay} ${isCart ? 'overlay-open' : ''}`}>
+                                <div className={`${s.cart_main} ${isCart ? 'cart-open' : ''}`}>
+                                    <div className={s.cart_top}>
+                                        <h3>Корзина</h3>
+
+                                        <svg onClick={cartClosed} aria-hidden="true" focusable="false" fill="none" width="14" class="icon icon-close" viewBox="0 0 16 16">
+                                            <path d="m1 1 14 14M1 15 15 1" stroke="currentColor" stroke-width="1.5"></path>
+                                        </svg>
+                                    </div>
+
+                                    <div className={s.cart_list}>
+                                        <div className={s.cart_item}>
+                                            <img src="/card-1.jpeg" alt="img" />
+
+                                            <div className={s.cart_box}>
+                                                <h3>Morgan's pomade</h3>
+                                                <p>50ml</p>
+                                                <b>15 000 sum</b>
+
+                                                <div className={s.flex}>
+                                                    <div className={s.row}>
+                                                        <button>-</button>
+                                                        <p>1</p>
+                                                        <button>+</button>
+                                                    </div>
+
+                                                    <button className={s.remove}>Remove</button>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div className={s.cart_item}>
+                                            <img src="/card-1.jpeg" alt="img" />
+
+                                            <div className={s.cart_box}>
+                                                <h3>Morgan's pomade</h3>
+                                                <p>50ml</p>
+                                                <b>15 000 sum</b>
+
+                                                <div className={s.flex}>
+                                                    <div className={s.row}>
+                                                        <button>-</button>
+                                                        <p>1</p>
+                                                        <button>+</button>
+                                                    </div>
+
+                                                    <button className={s.remove}>Remove</button>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div className={s.cart_item}>
+                                            <img src="/card-1.jpeg" alt="img" />
+
+                                            <div className={s.cart_box}>
+                                                <h3>Morgan's pomade</h3>
+                                                <p>50ml</p>
+                                                <b>15 000 sum</b>
+
+                                                <div className={s.flex}>
+                                                    <div className={s.row}>
+                                                        <button>-</button>
+                                                        <p>1</p>
+                                                        <button>+</button>
+                                                    </div>
+
+                                                    <button className={s.remove}>Remove</button>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div className={s.cart_item}>
+                                            <img src="/card-1.jpeg" alt="img" />
+
+                                            <div className={s.cart_box}>
+                                                <h3>Morgan's pomade</h3>
+                                                <p>50ml</p>
+                                                <b>15 000 sum</b>
+
+                                                <div className={s.flex}>
+                                                    <div className={s.row}>
+                                                        <button>-</button>
+                                                        <p>1</p>
+                                                        <button>+</button>
+                                                    </div>
+
+                                                    <button className={s.remove}>Remove</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div className={s.cart_form}>
+                                        <form action="">
+                                            <input placeholder='Имя' type="text" />
+                                            <input placeholder='Телефон' type="text" />
+                                            <button>Оформить заказ</button>
+                                        </form>
+                                    </div>
+
+                                </div>
+
+                            </div>
                         </div>
 
                         <div className={`${s.burger} ${isMenu ? 'burger-open' : ''}`} onClick={(e) => menuHandle(e)}>

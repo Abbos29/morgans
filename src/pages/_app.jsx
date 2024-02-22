3c8cec861c79ad362d5772acdedba03fab96f223
Abbos29 <abbosvaliev2003@gmail.com>
2024-02-22 14:01:35 +0500

    feat: add n-progress

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -3,12 +3,14 @@ import '@/styles/reset.scss';
 import '@/styles/globals.scss';
 import Footer from '@/components/Footer/Footer';
 import { SWRConfig } from 'swr';
+import NextNProgress from 'nextjs-progressbar';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <SWRConfig>
         <Header />
+        <NextNProgress color='black' />
         <Component {...pageProps} />
         <Footer />
       </SWRConfig>

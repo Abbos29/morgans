7ba3ad49cb22292902d70b6a1c7ed304fdb61695
BoburXO <boburhatamov7@gmail.com>
2024-05-30 17:12:41 +0500

    feat: pagination filter added

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 import Product from '../Product/Product';
 import s from './Products.module.scss';
 import useSWR from 'swr';
 import Filter from '../Filter/Filter';
-import Loader from '../Loader/Loader';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
 const Products = () => {
-  const { data } = useSWR('https://api.morgans-store.uz/products/', fetcher);
+  const router = useRouter();
+  const [currentPage, setCurrentPage] = useState(1);
 
-  // if (!data?.results?.length) return <Loader />;
+  // Get current page from query params on initial load
+  useEffect(() => {
+    const page = parseInt(router.query.page, 10) || 1;
+    setCurrentPage(page);
+  }, [router.query.page]);
+
+  const url = `https://api.morgans-store.uz/products/?page=${currentPage}`;
+  const { data } = useSWR(url, fetcher);
+
+  const handlePageChange = (event, page) => {
+    setCurrentPage(page);
+    router.push({ pathname: router.pathname, query: { ...router.query, page } });
+  };
 
   return (
     <>
@@ -25,6 +40,17 @@ const Products = () => {
               ))}
             </div>
           </div>
+          {/* Pagination */}
+          <div className={s.pagination}>
+            <Stack spacing={2}>
+              <Pagination
+                count={data?.total_pages || 1}
+                page={currentPage}
+                onChange={handlePageChange}
+              />
+            </Stack>
+          </div>
+          {/* Pagination */}
         </div>
       </section>
     </>
